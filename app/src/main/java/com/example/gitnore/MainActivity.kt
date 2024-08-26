82e4441803d68f3106f0cd2a1aa5dc2b555c5346
kevinlizandrousccausca <kevin.uscca@tecsup.edu.pe>
2024-08-26 18:28:47 -0500

    feat:Navigation

--- app/src/main/java/com/example/gitnore/MainActivity.kt
+++ app/src/main/java/com/example/gitnore/MainActivity.kt
@@ -4,23 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.filled.Favorite
+
+import androidx.compose.material.icons.filled.Place
+
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Snackbar
+
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
+
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 import com.example.gitnore.ui.theme.GitnoreTheme
+import java.lang.reflect.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +32,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GitnoreTheme {
-                 SnackbarControl()
+                 NavigationRailControl()
 
 
             }
@@ -37,19 +41,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun SnackbarControl() {
-    var showSnackbar by remember { mutableStateOf(false) }
-    Button(onClick = { showSnackbar = true }) {
-        Text("Snackbar")
-    }
-    if (showSnackbar) {
-        Snackbar(
-            action = {
-                Button(onClick = { showSnackbar = false }) {
-                    Text("Dismiss")
-                }
-            }
-        ) { Text("This is a Snackbar.") }
+fun NavigationRailControl() {
+    NavigationRail(
+
+
+    ) {
+        NavigationRailItem(
+            selected = true,
+            onClick = {},
+            label = { Text("NavigationRail") },
+            icon = { Icon(Icons.Filled.Favorite, contentDescription = "Favorites") }
+        )
+        NavigationRailItem(
+            selected = false,
+            onClick = {},
+            label = { Text("NavigationRail") },
+            icon = { Icon(Icons.Filled.Place, contentDescription = "Places") }
+        )
     }
 }
 
@@ -58,5 +66,5 @@ fun SnackbarControl() {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    SnackbarControl()
+    NavigationRailControl()
 }
