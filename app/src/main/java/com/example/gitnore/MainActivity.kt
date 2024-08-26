120585b06706bbf79db8cb8ff53f198417cd8b38
kevinlizandrousccausca <kevin.uscca@tecsup.edu.pe>
2024-08-26 18:23:37 -0500

    feat:Snacbar

--- app/src/main/java/com/example/gitnore/MainActivity.kt
+++ app/src/main/java/com/example/gitnore/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Snackbar
+
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+
 import com.example.gitnore.ui.theme.GitnoreTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +28,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GitnoreTheme {
-                ScaffoldExample()
+                 SnackbarControl()
 
 
             }
@@ -28,18 +37,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ScaffoldExample() {
-    Scaffold(
-        topBar = {
-            Text("Top Bar", modifier = Modifier.padding(16.dp))
-        }
-    ) { paddingValues ->
-        Text("Content", modifier = Modifier.padding(paddingValues))
+fun SnackbarControl() {
+    var showSnackbar by remember { mutableStateOf(false) }
+    Button(onClick = { showSnackbar = true }) {
+        Text("Snackbar")
+    }
+    if (showSnackbar) {
+        Snackbar(
+            action = {
+                Button(onClick = { showSnackbar = false }) {
+                    Text("Dismiss")
+                }
+            }
+        ) { Text("This is a Snackbar.") }
     }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ScaffoldExample()
+    SnackbarControl()
 }
